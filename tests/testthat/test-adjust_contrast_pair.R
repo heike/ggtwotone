0ee214addaa1def990e00fddf5b543fe312edc2c
bwanniarachchige2 <bwanniarachchige2@huskers.unl.edu>
2025-07-29 17:49:15 -0500

    add more tests for error handling

--- tests/testthat/test-adjust_contrast_pair.R
+++ tests/testthat/test-adjust_contrast_pair.R
@@ -17,3 +17,25 @@ test_that("adjust_contrast_pair fails silently when quiet = TRUE", {
   expect_true(is.na(result$contrast))
 })
 
+test_that("adjust_contrast_pair warns and falls back on invalid background", {
+  expect_warning(
+    result <- adjust_contrast_pair("#777777", background = "#ZZZZZZ", quiet = FALSE),
+    regexp = "Invalid background color"
+  )
+
+  expect_equal(result$light, "#FFFFFF")
+  expect_equal(result$dark, "#000000")
+  expect_true(is.na(result$contrast))
+})
+
+test_that("adjust_contrast_pair falls back silently when quiet = TRUE", {
+  expect_silent(
+    result <- adjust_contrast_pair("#777777", background = "#ZZZZZZ", quiet = TRUE)
+  )
+
+  expect_equal(result$light, "#FFFFFF")
+  expect_equal(result$dark, "#000000")
+  expect_true(is.na(result$contrast))
+})
+
+
