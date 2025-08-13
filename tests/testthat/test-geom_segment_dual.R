021fecb157de8db6dbbd4e6430b40bbe743bc5b5
bwanniarachchige2 <bwanniarachchige2@huskers.unl.edu>
2025-08-13 13:52:17 -0500

    Updated test with 'colour'

--- tests/testthat/test-geom_segment_dual.R
+++ tests/testthat/test-geom_segment_dual.R
@@ -1,6 +1,10 @@
 test_that("geom_segment_dual throws error with missing aesthetics", {
   p <- ggplot2::ggplot(data.frame(x = 1:3, y = 1:3)) +
-    geom_segment_dual(aes(x = x, y = y))  # missing xend/yend
+    geom_segment_dual(
+      aes(x = x, y = y),
+      colour1 = "black",
+      colour2 = "gray",
+      linewidth = 2)  # missing xend/yend
   expect_error(ggplot2::ggplot_build(p), regexp = "xend")
 })
 
@@ -8,8 +12,8 @@ test_that("geom_segment_dual works with minimal valid input", {
   p <- ggplot2::ggplot() +
     geom_segment_dual(
       aes(x = 1, y = 1, xend = 2, yend = 2),
-      color1 = "#FF0000",
-      color2 = "#000000",
+      colour1 = "#FF0000",
+      colour2 = "#000000",
       linewidth = 1
     )
   expect_silent(ggplot2::ggplot_build(p))
@@ -19,8 +23,8 @@ test_that("geom_segment_dual handles NA color1 without warning", {
   p <- ggplot2::ggplot() +
     geom_segment_dual(
       aes(x = 1, y = 1, xend = 2, yend = 2),
-      color1 = NA,
-      color2 = "#000000",
+      colour1 = NA,
+      colour2 = "#000000",
       linewidth = 1
     )
 
